036bbb35bc2f328a63fd5f50a307e1b34ba47f03
Martin Moldsvor <martin@moldsvor.com>
2019-09-04 09:17:53 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sorting_algorithms',
+      version='0.1',
+      description='Package for visualizing sorting algorithms',
+      url='http://github.com/storborg/funniest',
+      author='Martin Moldsvor',
+      author_email='martin@moldsvor.com',
+      license='MIT',
+      packages=['sorting_algorithm'],
+      zip_safe=False)
